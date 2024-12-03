bd29c3683a492857723b93b36025b7580d3bc3b3
Anna Makarenko <manahafor4@gmail.com>
2024-12-03 09:39:51 +0300

    Review fixes

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -17,8 +17,8 @@ export REPO_PATH_FILE="${REPO_PATH_FILE:-${METASYSTEM_PATH}/repo.path}"
 export PLATFORM_PATH="${PLATFORM_PATH:-${METASYSTEM_PATH}/ostis-web-platform}"
 
 export SC_WEB_REPO="${SC_WEB_REPO:-https://github.com/ostis-ai/sc-web.git}"
-export SC_WEB_BRANCH="${SC_WEB_BRANCH:-feat/nvm_for_macOS}"
-export SC_WEB_COMMIT="${SC_WEB_COMMIT:-""}"
+export SC_WEB_BRANCH="${SC_WEB_BRANCH:-main}"
+export SC_WEB_COMMIT="${SC_WEB_COMMIT:-"17aa0ca9912021119c2a67ba70e355c8dff35082"}"
 
 export SC_MACHINE_REPO="${SC_MACHINE_REPO:-https://github.com/ostis-ai/sc-machine.git}"
 export SC_MACHINE_BRANCH="${SC_MACHINE_BRANCH:-main}"
